e20708feffc10a3f5ce73b3c81093784302ca21d
Paul Maltsev <maltsevp@gmail.com>
2020-10-24 16:26:26 -0700

    adding fav-context to index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { FavProvider } from "./components/favorites/favorites-context";
+
 
 import App from "./components/app";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <CSSReset />
-        <App />
-      </ThemeProvider>
-    </Router>
+    <FavProvider>
+      <Router>
+        <ThemeProvider>
+          <CSSReset />
+          <App />
+        </ThemeProvider>
+      </Router>
+    </FavProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
